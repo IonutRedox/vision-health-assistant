484a8d544719c9046e0664c0320da36d52bf3347
IonutRedox <ionut.mcinjau@gmail.com>
2019-06-03 01:32:51 +0300

    Added side menu to UI

---
+++ UIVisionHealthAssistant/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIVisionHealthAssistant.ViewModel
+{
+    public class MainViewModel
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Get the application name.
+        /// </summary>
+        public string AppName { get; } = "Vision Health Assistant";
+
+        #endregion
+    }
+}
