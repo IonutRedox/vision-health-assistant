16695d814aae54817e553868ae3c6b430f105f0f
IonutRedox <ionut.mcinjau@gmail.com>
2019-08-09 22:42:56 +0300

    Implemented UI for break timer

--- UIVisionHealthAssistant/ViewModel/BreakTimerViewModel.cs
+++ UIVisionHealthAssistant/ViewModel/BreakTimerViewModel.cs
@@ -1,10 +1,15 @@
-﻿
-using VisionHealthAssistant.Shared;
+﻿using VisionHealthAssistant.Shared;
 
 namespace UIVisionHealthAssistant.ViewModel
 {
     public class BreakTimerViewModel : ViewModelBase
     {
+        #region Fields 
+
+        public string Info { get; } = "    Break timer notifies you to take regular breaks at specified time intervals to maintain eyes health.Please consider the minute as unit of measurement.";
+
+        #endregion
+
         #region Constructor
 
         public BreakTimerViewModel()
