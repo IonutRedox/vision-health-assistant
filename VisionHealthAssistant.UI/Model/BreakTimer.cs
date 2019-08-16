8759f9e60307a27b62a0bf0e495f5f9b04916e25
IonutRedox <ionut.mcinjau@gmail.com>
2019-08-17 00:02:05 +0300

    Established connection between Model-View-ViewModel for BreakTimer

---
+++ VisionHealthAssistant.UI/Model/BreakTimer.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VisionHealthAssistant.UI.Model
+{
+    public class BreakTimer : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private int _frequency;
+        private int _length;
+        private int _idleResetTime;
+        private string _message;
+        private bool _isPlaySoundActive;
+        private bool _isIdleResetActive;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets break frequency
+        /// </summary>
+        public int Frequency
+        {
+            get { return _frequency; }
+            set {
+                if ( _frequency != value ) {
+                    _frequency = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets break length
+        /// </summary>
+        public int Length
+        {
+            get { return _length; }
+            set {
+                if ( _length != value ) {
+                    _length = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets idle reset time
+        /// </summary>
+        public int IdleResetTime
+        {
+            get { return _idleResetTime; }
+            set {
+                if ( _idleResetTime != value ) {
+                    _idleResetTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets break message
+        /// </summary>
+        public string Message
+        {
+            get { return _message; }
+            set {
+                if ( _message != value ) {
+                    _message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets break sound play state
+        /// </summary>
+        public bool IsPlaySoundActive
+        {
+            get { return _isPlaySoundActive; }
+            set {
+                if ( _isPlaySoundActive != value ) {
+                    _isPlaySoundActive = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets break idle reset state
+        /// </summary>
+        public bool IsIdleResetActive
+        {
+            get { return _isIdleResetActive; }
+            set {
+                if ( _isIdleResetActive != value ) {
+                    _isIdleResetActive = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
--- VisionHealthAssistant.UI/ViewModel/BreakTimerViewModel.cs
+++ VisionHealthAssistant.UI/ViewModel/BreakTimerViewModel.cs
@@ -1,22 +1,43 @@
 ﻿using VisionHealthAssistant.Shared;
+using VisionHealthAssistant.UI.Model;
 
 namespace VisionHealthAssistant.UI.ViewModel
 {
     public class BreakTimerViewModel : ViewModelBase
     {
-        #region Fields 
-
-        public string Info { get; } = "    Break timer notifies you to take regular breaks at specified time intervals to maintain eyes health.Please consider the minute as unit of measurement.";
-
-        #endregion
-
         #region Constructor
 
         public BreakTimerViewModel()
         {
             Type = PageType.BreakTimer;
+            BreakTimer = new BreakTimer {
+                Frequency = 0,
+                Length = 0,
+                IdleResetTime = 0
+            };
         }
 
         #endregion
+
+        #region Fields 
+
+
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the page info.
+        /// </summary>
+        public string Info { get; } = "    Break timer notifies you to take regular breaks at specified time intervals to maintain eyes health.Please consider the minute as unit of measurement.";
+
+        /// <summary>
+        /// Gets the break timer.
+        /// </summary>
+        public BreakTimer BreakTimer { get; }
+
+        #endregion
+
     }
 }
