99450963a97b84cf437cc27919588c97b5c95471
IonutRedox <ionut.mcinjau@gmail.com>
2019-09-22 13:45:32 +0300

    Hidden relaxation window from ALT-TAB switcher

--- VisionHealthAssistant.UI/View/RelaxationView.xaml.cs
+++ VisionHealthAssistant.UI/View/RelaxationView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using VisionHealthAssistant.UI.Helper;
 
 namespace VisionHealthAssistant.UI.View
 {
@@ -10,6 +13,16 @@ namespace VisionHealthAssistant.UI.View
         public RelaxationView()
         {
             InitializeComponent();
+        }
+
+        private void RelaxationWindow_Loaded(object sender,RoutedEventArgs e)
+        {
+            WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+
+            int exStyle = (int)WindowHelper.GetWindowLong(wndHelper.Handle,(int)WindowHelper.GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)WindowHelper.ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            WindowHelper.SetWindowLong(wndHelper.Handle,(int)WindowHelper.GetWindowLongFields.GWL_EXSTYLE,(IntPtr)exStyle);
         }
     }
 }
